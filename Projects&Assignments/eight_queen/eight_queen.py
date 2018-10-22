ea9f9479cda90b517bfb2af1c09c30f5cbd2bab4
HelenBai <baihelen1@gmail.com>
2018-10-22 19:36:20 +0800

    1

--- Projects&Assignments/eight_queen/eight_queen.py
+++ Projects&Assignments/eight_queen/eight_queen.py
@@ -17,14 +17,16 @@ def main():
     for i in range(k):
         list.append(i)
     found=0
-    q=eval(input("numbers of solution you want:"))
-    while found<q:
-        rd.shuffle(list)
-        if not has_clashes(list):
-            draw_board(list)
-            found=found+1
-
-def draw_board(list):
+    if k > 2:
+        q=eval(input("numbers of solution you want:"))
+        while found<q:
+            rd.shuffle(list)
+            if not has_clashes(list):
+                draw_board(list,k)
+                found=found+1
+    else:
+        draw_board(list,k)
+def draw_board(list,f):
     pg.init()
     screencaption = pg.display.set_caption('eight_queen')
     colors=[(255,0,0),(0,0,0)]
@@ -38,29 +40,41 @@ def draw_board(list):
     crown=pg.transform.scale(crown, (each, each))
     crown_offset=(each-crown.get_width())//2
 
-    while True:
-        ev = pg.event.poll()
-        if ev.type == pg.KEYDOWN:
-            if ev.key == pg.K_SPACE:
-                break
-        if ev.type == pg.QUIT:
-            sys.exit()
+    error=pg.image.load("error.png")
+    error=pg.transform.scale(error,(screensize,screensize))
+    if f < 3:
+        while True:
+            ev = pg.event.poll()
 
-        for i in range(num) :
-            k=i%2
-            for j in range(num):
-                pg.draw.rect(screen,colors[k], [i*each,j*each, each, each], 0)
-                if k ==0:
-                    k=k+1
-                else:
-                    k=k-1
+            if ev.type == pg.QUIT:
+                sys.exit()
+            screen.blit(error,(0,0))
 
-       # for i in range(len(list)):
-            #screen.blit(crown,i*each+crown_offset,list[i]*each+crown_offset)
-        for (col, row) in enumerate(list):
-            screen.blit(crown,
-                         (col * each + crown_offset, row * each + crown_offset))
+            pg.display.flip()
+    else:
+        while True:
+            ev = pg.event.poll()
+            if ev.type == pg.KEYDOWN:
+                if ev.key == pg.K_SPACE:
+                    break
+            if ev.type == pg.QUIT:
+                sys.exit()
 
-        pg.display.flip()
-    pg.quit()
+            for i in range(num) :
+                k=i%2
+                for j in range(num):
+                    pg.draw.rect(screen,colors[k], [i*each,j*each, each, each], 0)
+                    if k ==0:
+                        k=k+1
+                    else:
+                        k=k-1
+
+           # for i in range(len(list)):
+                #screen.blit(crown,i*each+crown_offset,list[i]*each+crown_offset)
+            for (col, row) in enumerate(list):
+                screen.blit(crown,
+                             (col * each + crown_offset, row * each + crown_offset))
+
+            pg.display.flip()
+        pg.quit()
 main()
