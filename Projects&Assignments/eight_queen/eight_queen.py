6498b8be95ae4a1436fb9e65112677d9f649b9f6
HelenBai <baihelen1@gmail.com>
2018-10-24 08:12:49 +0800

    1

--- Projects&Assignments/eight_queen/eight_queen.py
+++ Projects&Assignments/eight_queen/eight_queen.py
@@ -1,7 +1,31 @@
 import random as rd
 import pygame as pg
 import sys
-def has_clashes(list):
+def share_diagonal(x0, y0, x1, y1):
+    """ Is (x0, y0) on a shared diagonal with (x1, y1)? """
+    dy = abs(y1 - y0)        # Calc the absolute y distance
+    dx = abs(x1 - x0)        # CXalc the absolute x distance
+    return dx == dy          # They clash if dx == dy
+def col_clashes(bs, c):
+    """ Return True if the queen at column c clashes
+         with any queen to its left.
+    """
+    for i in range(c):     # Look at all columns to the left of c
+          if share_diagonal(i, bs[i], c, bs[c]):
+              return True
+
+    return False
+def has_clashes(the_board):
+    """ Determine whether we have any queens clashing on the diagonals.
+        We're assuming here that the_board is a permutation of column
+        numbers, so we're not explicitly checking row or column clashes.
+    """
+    for col in range(1,len(the_board)):
+        if col_clashes(the_board, col):
+            return True
+    return False
+
+def has_clash(list):
     k=0
     for i in range(len(list)):
         if i == list[i] or i ==list[-i-1]:
@@ -42,7 +66,7 @@ def draw_board(list,f):
 
     error=pg.image.load("error.png")
     error=pg.transform.scale(error,(screensize,screensize))
-    if f < 3:
+    if f < 3 :
         while True:
             ev = pg.event.poll()
 
