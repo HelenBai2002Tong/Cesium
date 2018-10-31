d04a7f3034a7cb9acf63ba27b29ac6dd87bc7741
HelenBai <baihelen1@gmail.com>
2018-10-31 11:31:56 +0800

    1

--- Projects&Assignments/Q1Oct31.py
+++
@@ -1,6 +0,0 @@
-def check(K,P):
-    while K > 1:
-        print(K)
-        K=K-1
-        P=P*K
-    print(P)
--- Projects&Assignments/testOct31.py
+++ Projects&Assignments/testOct31.py
@@ -1,3 +1,10 @@
+def check(K,P):
+    while K > 1:
+        print(K)
+        K=K-1
+        P=P*K
+    print(P)
+
 class magic_array(object):
     def __init__(self):
         pass
