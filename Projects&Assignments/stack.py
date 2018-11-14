8dfe8755cfd66fce9bb043859d30fdf4987587ef
HelenBai <baihelen1@gmail.com>
2018-11-14 13:59:35 +0800

    1

--- Projects&Assignments/stack.py
+++ Projects&Assignments/stack.py
@@ -42,3 +42,59 @@ def checkbracket(symbol):
         return False
 print(checkbracket('((()))'))
 print(checkbracket('(()'))
+
+def checkgeneral(symbol):
+    s=Stack()
+    o = ["(", "[", "{"]
+    def match(a,b):
+        o=["(","[","{"]
+        p=[")","]","}"]
+        i=o.index(a)
+        j=p.index(b)
+        return i == j
+    for i in symbol:
+        if i in o:
+            s.push(i)
+        else:
+            if s.IsEmpty()==True:
+                return False
+            else:
+                l=s.peek()
+                if match(l,i):
+                    s.pop()
+                else:
+                    return False
+    if s.IsEmpty():
+        return True
+    else:
+        return False
+
+print(checkgeneral('{{([][])}()}'))
+print(checkgeneral('[{()]'))
+
+def dectobi(number):
+    s=Stack()
+    while number >0:
+        s.push(number%2)
+        number=number//2
+    a=''
+    while not s.IsEmpty():
+        a=a+str(s.pop())
+    a=int(a)
+    return a
+
+print(dectobi(117))
+
+def convert(number,n):
+    digits="0123456789ABCDEFGHIJK"
+    s=Stack()
+    while number >0:
+        k=digits[number % n]
+        s.push(k)
+        number=number//n
+    a=''
+    while not s.IsEmpty():
+        a=a+str(s.pop())
+    return a
+
+print(convert(2020,16))
