76993a0becdcd0f0de8651111e308d14096c3e87
HelenBai <baihelen1@gmail.com>
2019-01-01 22:48:45 +0800

    1

--- Projects&Assignments/perception.py
+++ Projects&Assignments/perception.py
@@ -75,29 +75,39 @@ def f(x):
     '''
     return 1 if x > 0 else 0
 
-X1, y1 = np.random.uniform(-11,-7,200).reshape((100, 2)), np.zeros(100)
-X2, y2 = np.random.uniform(7,11,200).reshape((100, 2)), np.ones(100)
-X=X1+X2
-y=y1+y2
+# randomly generate 50 points
+X1, y1 = np.random.uniform(-11,-7,100).reshape((50, 2)), np.zeros(50)
+X2, y2 = np.random.uniform(7,11,100).reshape((50, 2)), np.ones(50)
+
+X_train=np.concatenate([X1[0:40],X2[0:40]])
+y_train=np.concatenate([y1[0:40],y2[0:40]])
+X_test=np.concatenate([X1[40:],X2[40:]])
+y_test=np.concatenate([y1[40:],y2[40:]])
 p=perceptron(2,f)
-p.train(X,y,100,0.1)
+p.train(X_train,y_train,100,0.1) # train the perceptron
+results=[]
+for i in range(20):
+    results.append(p.predict(X_test[i])) # to predict the result, the result should be 0 * 10 and 1 * 10
+print("result", results)
+print("Is the prediction right?", results==y_test)
 k1=[]
 k2=[]
 n1=[]
 n2=[]
 
-for i in range(100):
+for i in range(50):
     k1=k1+[X1[i][0]]
     k2=k2+[X1[i][1]]
     n1=n1+[X2[i][0]]
     n2=n2+[X2[i][1]]
 
-plt.scatter(k1,k2,marker="x")
-plt.scatter(n1,n2,marker="+")
-slope=-p.getWeight()[0]/p.getWeight()[1]
-intercept=-p.getBias()/p.getWeight()[1]
-x=np.linspace(-10,12,100)
-y=slope*x+intercept
+# draw the resulted graph
+plt.scatter(k1,k2,marker="x") # plot the points marked with 0
+plt.scatter(n1,n2,marker="+") # plot the points marked with 1
+slope=-p.getWeight()[0]/p.getWeight()[1] # calculate the slope of the line
+intercept=-p.getBias()/p.getWeight()[1] # calculate the interception with the y-axis
+x=np.linspace(-10,10,20)
+y=slope*x+intercept # the equation of the graph
 plt.plot(x,y)
 plt.show()
 
@@ -121,7 +131,19 @@ def and_perceptron():
     x,y=getanddata()
     for i in x:
         print("and gate",i,a.predict(i))
-
+    k1=[0,1,0]
+    k2=[0,0,1]
+    n1=1
+    n2=1
+    plt.scatter(k1, k2, marker="x")  # plot the points marked with 0
+    plt.scatter(n1, n2, marker="+")  # plot the points marked with 1
+    slope = -a.getWeight()[0] / a.getWeight()[1]  # calculate the slope of the line
+    intercept = -a.getBias() / a.getWeight()[1]  # calculate the interception with the y-axis
+    x = np.linspace(-1, 2, 5)
+    y = slope * x + intercept  # the equation of the graph
+    plt.plot(x, y)
+    plt.title("and gate")
+    plt.show()
 
 
 def or_perceptron():
@@ -135,15 +157,28 @@ def or_perceptron():
         x=[[1,1],[0,1],[1,0],[0,0]]
         y=[1,1,1,0]
         return x,y
-    def train_and():
+    def train_or():
         p=perceptron(2,f)
         x,y=getordata()
         p.train(x,y,10,0.1)
         return p
-    a=train_and()
+    a=train_or()
     x,y=getordata()
     for i in x:
         print("or gate",i,a.predict(i))
+    k1=0
+    k2=0
+    n1=[1,0,1]
+    n2=[1,1,0]
+    plt.scatter(k1, k2, marker="x")  # plot the points marked with 0
+    plt.scatter(n1, n2, marker="+")  # plot the points marked with 1
+    slope = -a.getWeight()[0] / a.getWeight()[1]  # calculate the slope of the line
+    intercept = -a.getBias() / a.getWeight()[1]  # calculate the interception with the y-axis
+    x = np.linspace(-1, 2, 5)
+    y = slope * x + intercept  # the equation of the graph
+    plt.plot(x, y)
+    plt.title("or gate")
+    plt.show()
 
 
 and_perceptron()
